f538fe61e5f77a55ee9bd5565d7df0779213a1ec
rodrigomartinezar <30805946+rodrigomartinezar@users.noreply.github.com>
2018-04-06 19:52:32 +0000

    Guia ejercicios 1 excepto funcion MERGE

--- fila_l.h
+++ fila_l.h
@@ -9,50 +9,105 @@ typedef struct node{
     struct node* next;
 }node;
 
-node* first = NULL;
+node* first1 = NULL;
+node* first2 = NULL;
 
-void QUEUE(int n){
-    node* ptrnew = (node*)malloc(sizeof(node));
-    ptrnew->n= n;
-    ptrnew->next=NULL;
-    if(first == NULL){
-        first = ptrnew;
+void QUEUE(int n, int lista) {
+    node *ptrnew = (node *) malloc(sizeof(node));
+    ptrnew->n = n;
+    ptrnew->next = NULL;
+    if (lista == 1) {
+        if (first1 == NULL) {
+            first1 = ptrnew;
+        } else if (ptrnew->n < first1->n) {
+            ptrnew->next = first1;
+            first1 = ptrnew;
+        } else {
+            node *ptr = first1;
+            node *tmp = first1;
+            for (;;) {
+                if (ptr->next == NULL) {
+                    tmp = ptr;
+                    ptr->next = ptrnew;
+                    break;
+                } else if (ptr->next->n > ptrnew->n) {
+                    ptrnew->next = ptr->next;
+                    ptr->next = ptrnew;
+                    break;
+                }
+                ptr = ptr->next;
+            }
+        }
     }
-    else{
-        node* tmp = first;
-        while(1>0){
-            if(tmp->next != NULL){
-                tmp = tmp->next;
-            }
-            if(tmp->next == NULL){
-                tmp->next=ptrnew;
-                break;
-            }
-        }}
-}
-
-int DEQUEUE(){
-    if(first != NULL){
-        int n;
-        node *tmp=first;
-        first = tmp->next;
-        n = tmp->n;
-        free(tmp);
-        return n;}
     else
-        return 0;
-
-
+        if(lista == 2){
+            if (first2 == NULL) {
+                first2 = ptrnew;
+            } else if (ptrnew->n < first2->n) {
+                ptrnew->next = first1;
+                first2 = ptrnew;
+            } else {
+                node *ptr = first2;
+                node *tmp = first2;
+                for (;;) {
+                    if (ptr->next == NULL) {
+                        tmp = ptr;
+                        ptr->next = ptrnew;
+                        break;
+                    } else if (ptr->next->n > ptrnew->n) {
+                        ptrnew->next = ptr->next;
+                        ptr->next = ptrnew;
+                        break;
+                    }
+                    ptr = ptr->next;
+                }
+            }}
+    else printf("\nOpcion invalida\n");
 }
 
-void PEEK(){
-    if(first != NULL)
-        printf("%i\n", first->n);
+int DEQUEUE(int lista) {
+    if (lista == 1) {
+        if (first1 != NULL) {
+            int n;
+            node *tmp = first1;
+            first1 = tmp->next;
+            n = tmp->n;
+            free(tmp);
+            return n;
+        } else
+            return 0;
+    }
+    else
+        if(lista == 2){
+            if (first2 != NULL) {
+                int n;
+                node *tmp = first2;
+                first2 = tmp->next;
+                n = tmp->n;
+                free(tmp);
+                return n;
+            } else
+                return 0;
+        }
+}
+
+void PEEK(int lista){
+    if(lista == 1){
+    if(first1 != NULL)
+        printf("%i\n", first1->n);
     else
         printf("Fila vacía\n");
+    }
+    else
+        if(lista == 2){
+            if(first2 != NULL)
+                printf("%i\n", first2->n);
+            else
+                printf("Fila vacía\n");
+        }
 }
 
-void ISEMPTY(){
+void ISEMPTY(node* first){
     if(first == NULL){
         printf("Fila vacía\n");
     }
@@ -62,6 +117,7 @@ void ISEMPTY(){
 }
 
 node* INVERTIR_LISTA(node* first){
+    if (first != NULL) {
         node *last = first;
         while (last->next != NULL) {
             last = last->next;
@@ -78,6 +134,8 @@ node* INVERTIR_LISTA(node* first){
         lasttmp->next = NULL;
         first = last;
         return first;
+    }
+    else printf("\nLista vacia\n"); return NULL;
 }
 
 void MOSTRAR_LISTA(node* first){
@@ -89,6 +147,51 @@ void MOSTRAR_LISTA(node* first){
         ptr = ptr->next;
     }
     printf("\n\n");
+}
+
+void ELIMINA_REPETIDOS(node* first){
+    node* tmp = first;
+    node* tmp2;
+    if(tmp == NULL) printf("\nLista vacia");
+    else{
+        if(tmp->next == NULL) printf("\nSolo hay un elemento en la lista\n");
+        else
+        if(tmp->next->n == tmp->n) tmp->next = NULL;
+        else {
+            for (;;) {
+                int numero_buscado = tmp->n;
+                tmp2 = tmp->next;
+                while(tmp2!=NULL){
+                    if(numero_buscado == tmp2->n){
+                        tmp->next = tmp2->next;
+                        tmp2 = tmp->next;
+                    }
+                    else{
+                        tmp2 = tmp2->next;
+                    }
+                }
+                tmp = tmp->next;
+                if(tmp->next == NULL) break;
+            }
+        }
+    }
+}
+
+int k_antes_del_ultimo(int k, node* first){
+    node* last = first;
+    node* tmp = first;
+    if(first == NULL) printf("\nLista vacia\n");
+    else
+        if(first->next == NULL) printf("\nSolo hay un elemento en la lista\n");
+    else{
+            while(last->next != NULL) last=last->next;
+            for(int i = 0; i<k; i++){
+                while(tmp->next != last) tmp = tmp->next;
+                last = tmp;
+                tmp = first;
+            }
+            return last->n;
+        }
 }
 
 
